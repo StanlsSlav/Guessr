9fb3da117df2837d9012a1f2dc2322bce55f55dd
StanlsSlav <StanlsSlav@users.noreply.github.com>
2021-03-07 17:09:21 +0100

    Rename namespace

--- DataParsing/ParseRequest.cs
+++ DataParsing/ParseRequest.cs
@@ -1,34 +1,33 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using static System.Net.WebUtility;
-using static OpenTriviaAPICaller.ErrorsDictionary;
+using static OpenTrivia.ErrorsDictionary;
 using static Newtonsoft.Json.JsonConvert;
 
-namespace OpenTriviaAPICaller
+namespace OpenTrivia
 {
 	class ParseRequest
 	{
 		private static readonly string _BaseUrl = "https://opentdb.com/api.php";
 
-		public static Trivia _Trivia;
+		public static Trivia Quizz;
 		public static async Task ParseQuestion()
 		{
-			var webResponse = await new HttpClient().GetStringAsync(_BaseUrl + FilterRequests.Options);
+			var webResponse = await new System.Net.Http.HttpClient().GetStringAsync(_BaseUrl + FilterRequests.Options);
 			var root = DeserializeObject<Root>(webResponse);
 
 			//Had returned an error?
 			await HandleResponseCode(root.ResponseCode);
 
 			//The API will always respond with 1 trivia object
-			_Trivia = root.Results[0];
+			Quizz = root.Results[0];
 
-			#region Bad Practice
+			#region Bad
 			//Html Decoding
-			_Trivia.CorrectAnswer = HtmlDecode(_Trivia.CorrectAnswer);
-			_Trivia.Question = HtmlDecode(_Trivia.Question);
+			Quizz.CorrectAnswer = HtmlDecode(Quizz.CorrectAnswer);
+			Quizz.Question = HtmlDecode(Quizz.Question);
 
-			for (int i = 0; i < _Trivia.IncorrectAnswers.Count; i++)
-				_Trivia.IncorrectAnswers[i] = HtmlDecode(_Trivia.IncorrectAnswers[i]);
+			for (int i = 0; i < Quizz.IncorrectAnswers.Count; i++)
+				Quizz.IncorrectAnswers[i] = HtmlDecode(Quizz.IncorrectAnswers[i]);
 			#endregion
 		}
 	}
