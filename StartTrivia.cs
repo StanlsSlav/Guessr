05cee3f537ebf4fa3b6f722760298afe94bbc29c
StanlsSlav <StanlsSlav@users.noreply.github.com>
2021-03-07 17:09:21 +0100

    Rename namespace

--- StartTrivia.cs
+++ StartTrivia.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
-using static OpenTriviaAPICaller.ParseRequest;
-using static OpenTriviaAPICaller.ProcessInput;
-using static OpenTriviaAPICaller.ColorFeedBack;
 using static System.Console;
 using static System.ConsoleColor;
+using static OpenTrivia.ParseRequest;
+using static OpenTrivia.ColorFeedBack;
 
-namespace OpenTriviaAPICaller
+namespace OpenTrivia
 {
 	class StartTrivia
 	{
@@ -23,19 +22,19 @@ namespace OpenTriviaAPICaller
 				///<placesTillCorrect>Sets the place of the right answer as an index</placesTillCorrect>
 				///<correctChoiceNr>Save the correct choice number as an non index</correctChoiceNr>
 				///<toRest>Fix the repeating of the last wrong answer</toRest>
-				var placesTillCorrect = new Random().Next(0, _Trivia.IncorrectAnswers.Count + 1);
+				var placesTillCorrect = new Random().Next(0, Quizz.IncorrectAnswers.Count + 1);
 				var correctChoiceNr = placesTillCorrect + 1;
 				var toRest = 0;
 
-				WriteLine(_Trivia.Question + '\n');
+				WriteLine(Quizz.Question + '\n');
 				var showPlaceNr = 1;
-				for (int i = 0; i <= _Trivia.IncorrectAnswers.Count; i++)
+				for (int i = 0; i <= Quizz.IncorrectAnswers.Count; i++)
 				{
 					var isPlaceForCorrect = placesTillCorrect == 0;
 
 					Colored(
-						input: isPlaceForCorrect ? _Trivia.CorrectAnswer.TrimStart() : _Trivia.IncorrectAnswers[i - toRest],
-						prefixToColor: $"{showPlaceNr})",
+						input: isPlaceForCorrect ? Quizz.CorrectAnswer.TrimStart() : Quizz.IncorrectAnswers[i - toRest],
+						prefixToColor: showPlaceNr + ")",
 						foreground: DarkYellow
 					);
 
@@ -49,9 +48,9 @@ namespace OpenTriviaAPICaller
 
 				//Count as correct if the input is a number and it's equal to the correct answer
 				if (int.TryParse(userInput, out int userIntInput) && userIntInput == correctChoiceNr)
-					userInput = _Trivia.CorrectAnswer;
+					userInput = Quizz.CorrectAnswer;
 
-				if (await CheckIfCorrect(userInput))
+				if (await ProcessInput.CheckIfCorrect(userInput))
 					Colored("Correct!", foreground: Green);
 
 				else
