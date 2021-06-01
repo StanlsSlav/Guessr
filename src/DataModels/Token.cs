a64e79b3b22a4f1bbfe2db1494f1291ab6706119
StanlsSlav <StanlsSlav@users.noreply.github.com>
2021-06-01 14:24:32 +0200

    Remove code smell

---
+++ src/DataModels/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace OpenTriviaAPICaller.src.DataModels
+{
+    public class ApiToken
+    {
+        [JsonPropertyName("response_code")] public int ResponseCode { get; set; }
+
+        [JsonPropertyName("response_message")] public string ResponseMessage { get; set; }
+
+        [JsonPropertyName("token")] public string Token { get; set; }
+
+        // Extra property to track the token timeout
+        public DateTime RequestDate { get; set; }
+    }
+}
