6f6e2f45fe63b19743d5b001e72f72c6c6750d0f
StanlsSlav <StanlsSlav@users.noreply.github.com>
2021-06-01 14:24:32 +0200

    Remove code smell

---
+++ src/DataParsing/ParseToken.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using OpenTriviaAPICaller.src.DataModels;
+using OpenTriviaAPICaller.src.DataParsing.HandleAPIErrors;
+using static System.Text.Json.JsonSerializer;
+
+namespace OpenTriviaAPICaller.src.DataParsing
+{
+    internal static class ParseApiToken
+    {
+        private const string BaseUrl = "https://opentdb.com/api_token.php?command=";
+        private static readonly HttpClient Client = new();
+
+        public static ApiToken ParsedToken = new();
+
+        public static async Task RetrieveToken()
+        {
+            var webContent = await Client.GetStringAsync(BaseUrl + "request");
+            ParsedToken = Deserialize<ApiToken>(webContent);
+
+            UpdateTime();
+            await File.WriteAllTextAsync("token.json",
+                Serialize(ParsedToken, new JsonSerializerOptions {WriteIndented = true}));
+        }
+
+        public static async Task ResetToken()
+        {
+            var webContent = await Client.GetStringAsync(BaseUrl + "reset&token=" + ParsedToken.Token);
+
+            UpdateTime();
+            await ErrorsDictionary.HandleResponseCode(Deserialize<ApiToken>(webContent)!.ResponseCode);
+        }
+
+        public static void LoadToken()
+        {
+            if (File.Exists("token.json"))
+                ParsedToken = Deserialize<ApiToken>(File.ReadAllText("token.json"));
+        }
+
+        private static void UpdateTime()
+        {
+            ParsedToken.RequestDate = DateTime.Now;
+        }
+    }
+}
---
+++ src/DataParsing/ProcessInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenTriviaAPICaller.src.UI;
+using static OpenTriviaAPICaller.src.DataParsing.ParseRequest;
+
+namespace OpenTriviaAPICaller.src.DataParsing
+{
+    internal static class ProcessInput
+    {
+	    /// <summary>
+	    ///     Checks if the user input is equal to the correct answer
+	    ///     Or if a custom command was inputted
+	    /// </summary>
+	    public static async Task<bool> CheckIfCorrect(string input)
+        {
+            // Custom console commands during the game
+            switch (input)
+            {
+                case "quit" or "q":
+                    Menu.Exit(0);
+                    break;
+                case "back" or "b":
+                    await Task.Run(Menu.Render);
+                    break;
+            }
+
+            // CapsLock forgotten
+            return string.Equals(input, Quiz.CorrectAnswer, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
