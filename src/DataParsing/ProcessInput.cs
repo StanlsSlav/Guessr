4a200c81eafe3205b0a24ade6c54ce7669d84e32
StanlsSlav <StanlsSlav@users.noreply.github.com>
2021-06-09 15:18:58 +0200

    Small visual fixes

--- src/DataParsing/ProcessInput.cs
+++ src/DataParsing/ProcessInput.cs
@@ -7,11 +7,11 @@ namespace OpenTriviaAPICaller.DataParsing
 {
     internal static class ProcessInput
     {
-	    /// <summary>
-	    ///     Checks if the user input is equal to the correct answer
-	    ///     Or if a custom command was inputted
-	    /// </summary>
-	    public static async Task<bool> CheckIfCorrect(string input)
+        /// <summary>
+        ///     Checks if the user input is equal to the correct answer
+        ///     Or if a custom command was inputted
+        /// </summary>
+        public static async Task<bool> CheckIfCorrect(string input)
         {
             // Custom console commands during the game
             switch (input)
