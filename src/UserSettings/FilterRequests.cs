ac3d8e782ad5b2ef64663b879156b706769f2eb0
StanlsSlav <StanlsSlav@users.noreply.github.com>
2022-05-30 20:09:19 +0200

    Make use of file scoped namespaces

--- src/UserSettings/FilterRequests.cs
+++ src/UserSettings/FilterRequests.cs
@@ -2,33 +2,32 @@
 using OpenTriviaAPICaller.DataModels;
 using static OpenTriviaAPICaller.UserSettings.UserOptions;
 
-namespace OpenTriviaAPICaller.UserSettings
+namespace OpenTriviaAPICaller.UserSettings;
+
+internal static class FilterRequests
 {
-    internal static class FilterRequests
+    public static string Options;
+
+    public static void Filter(ApiToken token)
     {
-        public static string Options;
+        Options = "?amount=1";
 
-        public static void Filter(ApiToken token)
+        if (UserOptions.Type != 0)
         {
-            Options = "?amount=1";
-
-            if (UserOptions.Type != 0)
-            {
-                Options += "&type=" + Enum.GetName(typeof(TypeChoices), UserOptions.Type)?.ToLower();
-            }
-            else if (Difficulty != 0)
-            {
-                Options += "&difficulty=" + Enum.GetName(typeof(DifficultyChoices), Difficulty)?.ToLower();
-            }
-            else if (Category != 0)
-            {
-                //Offset
-                Options += "&category=" + (Category + 9);
-            }
-            else if (token is not null)
-            {
-                Options += "&token=" + token.Token;
-            }
+            Options += "&type=" + Enum.GetName(typeof(TypeChoices), UserOptions.Type)?.ToLower();
+        }
+        else if (Difficulty != 0)
+        {
+            Options += "&difficulty=" + Enum.GetName(typeof(DifficultyChoices), Difficulty)?.ToLower();
+        }
+        else if (Category != 0)
+        {
+            //Offset
+            Options += "&category=" + (Category + 9);
+        }
+        else if (token is not null)
+        {
+            Options += "&token=" + token.Token;
         }
     }
 }
