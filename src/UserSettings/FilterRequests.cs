acae8c4fd5cca472c7b600bdc1af6cbd78f99ea3
StanlsSlav <StanlsSlav@users.noreply.github.com>
2022-06-03 17:36:41 +0200

    Add forgotten file scoped namespaces

--- src/UserSettings/FilterRequests.cs
+++ src/UserSettings/FilterRequests.cs
@@ -2,33 +2,32 @@
 using Guessr.Models;
 using static Guessr.UserSettings.UserOptions;
 
-namespace Guessr.UserSettings
+namespace Guessr.UserSettings;
+
+internal static class FilterRequests
 {
-    internal static class FilterRequests
+    public static string Options;
+
+    public static void Filter(ApiToken token)
     {
-        public static string Options;
+        Options = "?amount=1";
 
-        public static void Filter(ApiToken token)
+        if (UserOptions.Type != 0)
         {
-            Options = "?amount=1";
-
-            if (UserOptions.Type != 0)
-            {
-                Options += "&type=" + Enum.GetName(typeof(TypeChoices), UserOptions.Type)?.ToLower();
-            }
-            else if (Difficulty != 0)
-            {
-                Options += "&difficulty=" + Enum.GetName(typeof(DifficultyChoices), Difficulty)?.ToLower();
-            }
-            else if (Category != 0)
-            {
-                //Offset
-                Options += "&category=" + (Category + 9);
-            }
-            else if (token is not null)
-            {
-                Options += "&token=" + token.Token;
-            }
+            Options += "&type=" + Enum.GetName(typeof(TypeChoices), UserOptions.Type)?.ToLower();
+        }
+        else if (Difficulty != 0)
+        {
+            Options += "&difficulty=" + Enum.GetName(typeof(DifficultyChoices), Difficulty)?.ToLower();
+        }
+        else if (Category != 0)
+        {
+            //Offset
+            Options += "&category=" + (Category + 9);
+        }
+        else if (token is not null)
+        {
+            Options += "&token=" + token.Token;
         }
     }
 }
