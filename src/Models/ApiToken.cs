bf7f34f155106570126197b2f7d9582d04ad4ee7
StanlsSlav <StanlsSlav@users.noreply.github.com>
2022-08-10 21:04:31 +0200

    Release v2.0.0

---
+++ src/Models/ApiToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Guessr.Models;
+
+public class ApiToken
+{
+    [JsonPropertyName("response_code")]
+    public int ResponseCode { get; set; }
+
+    [JsonPropertyName("response_message")]
+    public string ResponseMessage { get; set; }
+
+    [JsonPropertyName("token")]
+    public string Token { get; set; }
+
+    public DateTime RequestDate { get; set; }
+}
